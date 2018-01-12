801d712f0ced4602f030da8c83791f6463275c41
marcush12 <marcos012santos@gmail.com>
2018-01-12 12:43:04 -0200

    update till delete product

---
+++ bootstrap/cache/2c6dfbb392ffd2ef6296d688bd07c78c31e01f35.php
@@ -0,0 +1,17 @@
+<?php $__env->startSection('title', 'Dashboard'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <div class="dashboard">
+        <div class="row expanded">
+            <h2>Dashboard</h2>
+            <form action="/admin" method="post" enctype="multipart/form-data">
+                <input name="product" value="testing">
+                <input type="file" name="image">
+                <input type="submit" value="Go" name="submit">
+            </form>
+            
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('admin.layout.base', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
